7f687d2f4b080a9c2d94de26f71578f1e6a15c92
CSHuy1611 <he186445caosyhuy@gmail.com>
2026-02-04 23:15:22 +0700

    Fish demo array

--- Assets/_Game_Array/_Scripts/PlayerController.cs
+++ Assets/_Game_Array/_Scripts/PlayerController.cs
@@ -1,76 +1,121 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Cài đặt tốc độ")]
+    [Header("Cài đặt chung")]
     public float moveSpeed = 5f;
-    [Header("Cài đặt nhảy")]
     public float jumpForce = 7f;
 
-    [Header("Danh sách Vũ khí")]
-    public GameObject[] weapons;
+    [Header("Hệ thống Kỹ Năng")]
+    public GameObject[] skillPrefabs;
+    public Transform firePoint; 
+
+    private int currentSkillIndex = 0; 
+
 
     private Rigidbody2D rb;
+    private Animator anim;
     private bool isGrounded = true;
-
-    private int currentWeaponIndex = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        UpdateWeaponDisplay();
+        anim = GetComponent<Animator>();
+        Debug.Log("Bắt đầu với kỹ năng: " + currentSkillIndex);
     }
 
     void Update()
     {
+
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+        if (moveInput > 0) transform.localScale = new Vector3(1.2f, 1.2f, 1);
+        else if (moveInput < 0) transform.localScale = new Vector3(-1.2f, 1.3f, 1);
+
+        if (anim != null) anim.SetFloat("Speed", Mathf.Abs(moveInput));
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false; 
+            isGrounded = false;
+            anim.SetBool("IsGrounded", false);
         }
+
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            SwitchWeapon(); 
+            ChangeSkill();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Attack();
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        isGrounded = true;
-    }
-    private void UpdateWeaponDisplay()
-    {
-        for (int i = 0; i < weapons.Length; i++)
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
         {
-            if (i == currentWeaponIndex)
-                weapons[i].SetActive(true);
-            else
-                weapons[i].SetActive(false);
+            isGrounded = true;
+            anim.SetBool("IsGrounded", true);
         }
     }
-    private void SwitchWeapon()
+
+
+    void ChangeSkill()
     {
-        weapons[currentWeaponIndex].SetActive(false);
 
-        currentWeaponIndex++;
+        currentSkillIndex++;
 
-        if (currentWeaponIndex >= weapons.Length)
+
+        if (currentSkillIndex >= skillPrefabs.Length)
         {
-            currentWeaponIndex = 0; 
+            currentSkillIndex = 0;
         }
 
-        weapons[currentWeaponIndex].SetActive(true);
 
-        Debug.Log("Đang dùng vũ khí số: " + currentWeaponIndex);
+        if (currentSkillIndex == 0) GetComponent<SpriteRenderer>().color = Color.white; 
+        else GetComponent<SpriteRenderer>().color = Color.red;   
+
+        Debug.Log("Đã đổi sang kỹ năng số: " + currentSkillIndex);
+    }
+
+    void Attack()
+    {
+
+        anim.SetTrigger("Attack");
+
+
+        GameObject currentAttack = skillPrefabs[currentSkillIndex];
+
+        if (currentAttack == null)
+        {
+
+            Debug.Log("Chém thường!");
+
+        }
+        else
+        {
+
+            Debug.Log("Phóng Kiếm Khí!");
+            if (firePoint != null)
+            {
+                Quaternion bulletRotation;
+                if (transform.localScale.x > 0)
+                {
+                    bulletRotation = Quaternion.Euler(0, 0, 0);
+                }
+                else
+                {
+                    bulletRotation = Quaternion.Euler(0, 180, 0);
+                }
+                Instantiate(currentAttack, firePoint.position, bulletRotation);
+            }
+        }
     }
 }
